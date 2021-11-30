a8270ec69b8fa835accd1018adbb54169e077869
shiviishiv7 <shiviishiv7@gmail.com>
2021-11-30 16:17:48 +0545

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CopyRightComponent } from './copy-right/copy-right.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'contact-us',
+    component:ContactUsComponent
+  },
+  {
+    path:'privacy-policy',
+    component:PrivacyComponent
+  },
+  {
+    path:'copy-right',
+    component:CopyRightComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CopyRightComponent } from './copy-right/copy-right.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    PrivacyComponent,
+    ContactUsComponent,
+    CopyRightComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
